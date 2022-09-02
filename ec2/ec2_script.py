1f2eb77d32e5c51e1e7b81ec7b5b49523a8b1382
Yogita Surve <yogita.surve@medly.com>
2022-09-03 00:11:15 +0530

    Display EC2 Instance ID, Instance name, Region and Image ID

--- ec2/ec2_script.py
+++ ec2/ec2_script.py
@@ -1,12 +1,14 @@
 import boto3
 
+my_session = boto3.session.Session()
 client = boto3.client('ec2')
 response = client.describe_instances()
-tab = "\t"
-print("InstanceId"+tab+tab+"InstanceName")
+my_region = my_session.region_name
+print("{0:20} {1:30} {2:20} {3:10}".format("InstanceId", "InstanceName", "Region", "ImageId"))
 for instance in response["Reservations"]:
     instanceId = instance['Instances'][0]['InstanceId']
+    imageId = instance['Instances'][0]['ImageId']
     for tags in instance['Instances'][0]['Tags']:
         if tags['Key'] == 'Name':
             name = tags['Value']
-            print(instanceId+tab+name)
+            print("{0:20} {1:30} {2:20} {3:10}".format(instanceId, name, my_region, imageId))
