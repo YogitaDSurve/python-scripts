fa9df753395e698e5a5b01d7f4b04043d21ab131
Yogita Surve <yogita.surve@medly.com>
2022-09-05 12:30:11 +0530

    Display EC2 Instance ID, Instance name, Region, Image ID, private IP and Public IP

--- ec2/ec2_script.py
+++ ec2/ec2_script.py
@@ -4,11 +4,17 @@ my_session = boto3.session.Session()
 client = boto3.client('ec2')
 response = client.describe_instances()
 my_region = my_session.region_name
-print("{0:20} {1:30} {2:20} {3:10}".format("InstanceId", "InstanceName", "Region", "ImageId"))
+print("{0:20} {1:30} {2:10} {3:30} {4:30} {5:10}".format("InstanceId", "InstanceName", "Region", "ImageId", "PrivateIp", "PublicIP"))
 for instance in response["Reservations"]:
-    instanceId = instance['Instances'][0]['InstanceId']
-    imageId = instance['Instances'][0]['ImageId']
-    for tags in instance['Instances'][0]['Tags']:
+    instances = instance['Instances'][0]
+    instanceId = instances['InstanceId']
+    imageId = instances['ImageId']
+    privateIp = instances['PrivateIpAddress']
+    publicIp = "NA"
+    if "PublicIpAddress" in instances.keys():
+        publicIp = instance['Instances'][0]['PublicIpAddress']
+
+    for tags in instances['Tags']:
         if tags['Key'] == 'Name':
             name = tags['Value']
-            print("{0:20} {1:30} {2:20} {3:10}".format(instanceId, name, my_region, imageId))
+            print("{0:20} {1:30} {2:10} {3:30} {4:30} {5:10}".format(instanceId, name, my_region, imageId, privateIp, publicIp))
